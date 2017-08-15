f69f995885675f750bead6249e7e1495a9bf5241
Lin Tao <lintaoo@hotmail.com>
2017-08-15 17:44:59 -0400

    Initial commit

---
+++ point.py
@@ -0,0 +1,16 @@
+class Point2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+    def __repr__(self):
+        return 'Point2D(x={}, y={})'.format(self.x, self.y)
+
+    def __format__(self, f):
+        if f == 'r':
+            return '{}, {}'.format(self.y, self.x)
+        else:
+            return '{}, {}'.format(self.x, self.y)
